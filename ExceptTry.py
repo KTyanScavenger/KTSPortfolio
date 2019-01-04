2923f60b5316f83df56f8bcabfd12af3af467620
KTyanScavenger <kylabug@live.com>
2019-01-04 12:49:17 -0700

    Update ExceptTry.py

--- ExceptTry.py
+++ ExceptTry.py
@@ -1,13 +1,16 @@
 #Kyla Ryan
 #1/19
-
+##Try to give erors without the actual error
 while True:
     try:
+        #Ask for a number from the user
         num=float(input("Enter a number: "))
         break
     except ValueError:
+        #This gives the exception of a character that is not a number. 
          print("You must enter a number. ")
     except TypeError:
+        #Incorrect input somewhere
         print("Something went wrong.")
 print(num)
 for value in (None,"Hi!"):
@@ -18,12 +21,13 @@ for value in (None,"Hi!"):
         print("You must enter a number")
     except TypeError:
         print("Something went wrong.")
-
+#The previous code attempts to convert the value into floats. 
 try:
     num=float(input("\nEnter a number: "))
 except ValueError:
     print("That was not a number! Or as python would say...")
     print(e)
+    #This allows the user to see the actual error with a simplified version. 
 try:
     num=float(input("\nEnter a number: "))
 except ValueError:
